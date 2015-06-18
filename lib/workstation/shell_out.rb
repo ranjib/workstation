9865b015b12507b7a93af78d1143c27c12d406de
Ranjib Dey <ranjib@pagerduty.com>
2015-06-18 02:52:24 -0700

    fix require statement

--- lib/workstation/shell_out.rb
+++ lib/workstation/shell_out.rb
@@ -1,3 +1,5 @@
+require 'mixlib/shellout'
+
 module Workstation
   module ShellOut
     def shell_out(command)
@@ -9,7 +11,7 @@ module Workstation
 
     def shell_out!(command)
       cmd = shell_out(command)
-      fail "Failed to execute :' #{command}'" unless cmd.exitstatus.zero?
+      fail "Failed to execute : '#{command}'" unless cmd.exitstatus.zero?
     end
   end
 end
