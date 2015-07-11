ab0815ed96f950d58336cd20046a5481b869fb94
Ranjib Dey <ranjib@linux.com>
2015-07-11 09:35:50 -0700

    update vagrant

--- site-cookbooks/workstation/attributes/default.rb
+++ site-cookbooks/workstation/attributes/default.rb
@@ -1,4 +1,4 @@
 default['workstation']['user'] = 'ranjib'
 default['workstation']['home'] = '/home/ranjib'
-default['vagrant']['url'] = 'http://files.vagrantup.com/packages/a7853fe7b7f08dbedbc934eb9230d33be6bf746f/vagrant_1.2.1_x86_64.deb'
+default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.3_x86_64.deb'
 default['vagrant']['checksum'] = nil
